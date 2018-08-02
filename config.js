19b06013ad87838248ea9a3dfd2a459a8d3aedbf
Andrée Hansson <peolanha@gmail.com>
2018-08-02 22:08:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
     'readline', // ensure gawk gets good readline
     'gawk',
     // http://www.lcdf.org/gifsicle/ (because I'm a gif junky)
-    'gifsicle',
+    //'gifsicle',
     'gnupg',
     // Install GNU `sed`, overwriting the built-in `sed`
     // so we can do "sed -i 's/foo/bar/' file" instead of "sed -i '' 's/foo/bar/' file"
@@ -21,11 +21,11 @@ module.exports = {
     // better, more recent grep
     'homebrew/dupes/grep',
     // https://github.com/jkbrzt/httpie
-    'httpie',
+    //'httpie',
     // jq is a sort of JSON grep
     'jq',
     // Mac App Store CLI: https://github.com/mas-cli/mas
-    'mas',
+    //'mas',
     // Install some other useful utilities like `sponge`
     'moreutils',
     'nmap',
@@ -50,41 +50,41 @@ module.exports = {
     // 'box-sync',
     //'comicbooklover',
     //'diffmerge',
-    'docker', // docker for mac
+    //'docker', // docker for mac
     //'dropbox',
     //'evernote',
-    'flux',
+    //'flux',
     'gpg-suite',
     //'ireadfast',
-    'iterm2',
-    'little-snitch',
-    'macbreakz',
-    'micro-snitch',
-    'signal',
+    //'iterm2',
+    //'little-snitch',
+    //'macbreakz',
+    //'micro-snitch',
+    //'signal',
     //'macvim',
-    'sizeup',
+    //'sizeup',
     //'sketchup',
-    'slack',
+    //'slack',
     'the-unarchiver',
     //'torbrowser',
     //'transmission',
-    'visual-studio-code',
+    //'visual-studio-code',
     //'vlc',
-    'xquartz'
+    //'xquartz'
   ],
   gem: [
   ],
   npm: [
-    'antic',
-    'buzzphrase',
-    'eslint',
-    'instant-markdown-d',
+    //'antic',
+    //'buzzphrase',
+    //'eslint',
+    //'instant-markdown-d',
     // 'generator-dockerize',
     // 'gulp',
-    'npm-check-updates',
-    'prettyjson',
-    'trash',
-    'vtop'
+    //'npm-check-updates',
+    //'prettyjson',
+    //'trash',
+    //'vtop'
     // ,'yo'
   ]
 };
